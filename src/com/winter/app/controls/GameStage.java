59379af75af475aca643085ca47f4eb545e9b9b1
rlajinwon <wlsdnjs652@naver.com>
2025-01-03 09:10:11 +0900

    add

--- src/com/winter/app/controls/GameStage.java
+++ src/com/winter/app/controls/GameStage.java
@@ -1,5 +1,54 @@
 package com.winter.app.controls;
 
+import java.util.Random;
+
+import com.winter.app.units.Character;
+import com.winter.app.units.Monster;
+
 public class GameStage {
 
+	
+	private Random random;
+	
+	public GameStage() {
+		random = new Random();
+	}
+	
+	
+	public void fight(Character character) {
+		
+		Random random = new Random();
+		int count = random.nextInt(3)+1;
+		Monster[] monster = new Monster[count];
+		
+		for(int i = 0; i < monster.length; i++) {
+			
+			monster[i] = new Monster();
+		}
+		
+		this.attack(character,monster);
+	}
+	
+	private void attack(Character character, Monster[]monster) {
+		
+		Character ch = new Character();
+		
+		for(int i=0; i<monster.length; i++) {
+			int d = monster[i].getPower();
+			d = random.nextInt(d);
+			
+			character.setHp(character.getHp()-d);
+			
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
--- src/com/winter/app/units/Monster.java
+++ src/com/winter/app/units/Monster.java
@@ -2,4 +2,84 @@ package com.winter.app.units;
 
 public class Monster {
 
+	private String name;
+	private int level;
+	private int hp;
+	private int mp;
+	private int power;
+	private int exp;
+	
+	
+	public Monster() {
+		this.name = "슬라임";
+		this.level = 10;
+		this.hp = 100;
+		this.mp = 50;
+		this.power = 15;
+		this.exp = 20;
+		
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public int getHp() {
+		return hp;
+	}
+
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+
+	public int getMp() {
+		return mp;
+	}
+
+
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+
+
+	public int getPower() {
+		return power;
+	}
+
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+
+	public int getExp() {
+		return exp;
+	}
+
+
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+	
+	
 }
